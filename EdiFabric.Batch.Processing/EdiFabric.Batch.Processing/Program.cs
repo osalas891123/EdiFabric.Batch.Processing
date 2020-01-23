1b1763c46ef785aea5366a725b3a6ad754b5748b
randellluangrath <RNE6734@hca.corpad.net>
2020-01-23 11:19:37 -0600

    initial commit

---
+++ EdiFabric.Batch.Processing/EdiFabric.Batch.Processing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EdiFabric.Batch.Processing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
